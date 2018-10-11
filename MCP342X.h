c16f18b8a3982d700b2c4d087f820a7c7d36fd0d
Pavel Slama <p.slama@atlas.cz>
2018-10-11 23:15:12 +0200

    macro remove

--- MCP342X.h
+++ MCP342X.h
@@ -31,17 +31,6 @@ SOFTWARE.
 
 #define MCP342X_DEFAULT_ADDRESS 0x68 << 1
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0')
-
 class MCP342X {
   public:
     typedef enum {
